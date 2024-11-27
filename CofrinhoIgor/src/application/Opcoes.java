504e0a23c7393bd50a294c492d2e6e8c39a3550d
igorprnegrao <neurocientista@gmail.com>
2024-11-26 23:44:55 -0300

    MENU1

---
+++ CofrinhoIgor/src/application/Opcoes.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Opcoes {
+	
+	public void exibirOpcoes() {
+		System.out.println("COFRINHO DO IGOR");
+		System.out.println("1 - ADICIONAR MOEDA: ");
+		System.out.println("2 - REMOVER MOEDA: ");
+		System.out.println("3 - LISTAR MOEDAS: ");
+		System.out.println("4 - CONVERTER PARA REAL: ");
+		
+	}
+}
